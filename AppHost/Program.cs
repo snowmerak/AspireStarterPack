48253f49d54ec8b143ca73af1fdb17ef338e04a9
snowmerak <snowmerak@outlook.com>
2025-01-04 18:31:19 +0900

    make reverse proxy configurator

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -1,10 +1,25 @@
+using AppHost;
 using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<ReverseProxy>("ReverseProxy")
-    .WithExternalHttpEndpoints()
-    .WithHttpEndpoint(port: 3000, targetPort: 3000)
-    .WithHttpsEndpoint(port: 3030, targetPort: 3030);
+var reverseProxyConfigurator = new AppHost.ReverseProxy.Configurator(builder,
+    replica: 8,
+    port: 5000);
+
+var cache = builder.AddValkey(name: "SharedCache");
+
+var routeMap = new Dictionary<string, List<string>>();
+
+for (var i = 0; i < 32; i++)
+{
+    var name = $"EchoServer-{i}";
+    var echoServer = builder.AddGolangApp(name, "../EchoServer")
+        .WithHttpEndpoint(env: "PORT")
+        .WithReference(cache);
+    reverseProxyConfigurator.AddService("/", echoServer);
+}
+
+reverseProxyConfigurator.Build();
 
 builder.Build().Run();
